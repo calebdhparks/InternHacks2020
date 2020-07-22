a031aac83d74ebc3039f9d82b9001a0a46b7cb19
Caleb Parks <calebdhparks7@gmail.com>
2020-07-22 11:58:42 -0700

    update nav bar, started dashboard

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import { withAuthorization } from '../Session';
+import 'firebase/auth';
+import firebase from 'firebase';
 
-const HomePage = () => (
-  <div>
-    <h1>Home</h1>
-    <p>This is where the users will go after sign in</p>
-  </div>
-);
+class HomePage extends Component{
+  constructor(props){
+      super(props)
+      this.state = {
+          users:[],
+          loading:true
+      };
+      // this.getName=this.getName.bind(this)
+}
+componentDidMount() {
+  this.setState({ loading: true });
+
+  this.props.firebase.users().on('value', snapshot => {
+    const usersObject = snapshot.val();
+
+    const usersList = Object.keys(usersObject).map(key => ({
+      ...usersObject[key],
+      uid: key,
+    }));
+
+    this.setState({
+      users: usersList,
+      loading: false,
+    });
+  });
+}
+
+render(){
+  const {users,loading}=this.state;
+  console.log(this.state,users,loading)
+  return(
+    <h1>Hello, {!loading&&users[0].username}</h1>
+  )
+}
+}
+
+
+
+
+
 const condition = authUser => !!authUser;
 
 export default withAuthorization(condition)(HomePage);
+// getName(){
+//     firebase.auth().onAuthStateChanged(function(user) {
+//         if (user) { //User is authenticated and logged in
+//             // Get current uid
+//             var name = user.email;
+//             console.log(name);
+//             // this.setState({email:name}).bind(this)
+//             //alert(user.uid);
+//             // alert the user
+//           }
+//         }).bind(this);
+//       }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -16,36 +16,46 @@ const Navigation = () => (
 
   </div>
 );
-
+const menuStyle={
+  backgroundColor: '#eee'
+};
 const NavigationAuth = () => (
-  <ul className = "vertical-menu">
-    <li >
-      <Link to={ROUTES.LANDING}>Landing</Link>
+<div>
+  <ul className = "vertical-menu" style={menuStyle}>
+    <li>
+      <Link to={ROUTES.HOME}>My Dashboard</Link>
     </li>
     <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
+      <Link to={ROUTES.TRACK_MANAGE_APP}>Job Applications</Link>
     </li>
     <li>
       <Link to={ROUTES.JOB_SEARCH}>Job Search</Link>
     </li>
     <li>
-     <Link to={ROUTES.ADMIN}>Admin</Link>
+      <Link to={ROUTES.LANDING}>Goals and Rewards</Link>
     </li>
     <li>
-     <Link to={ROUTES.TRACK_MANAGE_APP}>Track and Manage Applications</Link>
+      <Link to={ROUTES.ACCOUNT}>My Profile</Link>
     </li>
     <li>
     <SignOutButton />
     </li>
   </ul>
+  <ul className = "vertical-menu"  style={menuStyle}>
+    <li>
+      <Link to={ROUTES.HOME}>Notifications</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Help & Support</Link>
+    </li>
+  </ul>
+  </div>
 
 );
 
 const NavigationNonAuth = () => (
-  <ul className = "vertical-menu">
+<div>
+  <ul className = "vertical-menu"  style={menuStyle}>
     <li>
       <Link to={ROUTES.LANDING}>Landing</Link>
     </li>
@@ -53,8 +63,17 @@ const NavigationNonAuth = () => (
       <Link to={ROUTES.SIGN_IN}>Sign In</Link>
     </li>
   </ul>
+  <ul className = "vertical-menu"  style={menuStyle}>
+    <li>
+      <Link to={ROUTES.HOME}>Help & Support</Link>
+    </li>
+  </ul>
+  </div>
 );
 
 export default Navigation;
 
 /*<Link to={ROUTES.SIGN_IN}><SignOutButton /></Link>*/
+// <li >
+//   <Link to={ROUTES.LANDING}>Landing</Link>
+// </li>
